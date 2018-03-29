90c8e024811eee2231f1de092c8c7466c525997d
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-03-29 11:27:41 -0300

    imported bootstrap & fontawesome

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all
+
+4.times do
+  Flat.create!(name: Faker::LordOfTheRings.location,
+    address: Faker::Address.street_address,
+    description: Faker::Company.catch_phrase,
+    price_per_night: rand(300),
+    number_of_guest: rand(4))
+end
